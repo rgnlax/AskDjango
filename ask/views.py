1c25cdb5e8dc6f41b82b23c2adffa11c0b175c2c
rgnlax <rgnlax96@gmail.com>
2015-04-30 15:53:44 +0300

    Pagination added and some design problems fixed

--- ask/views.py
+++ ask/views.py
@@ -1,25 +1,41 @@
 from django.shortcuts import render
 from ask.models import Question, Answer, Tag, Like, CustomUser
 from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 global_context = {"authorized" : True}
 
 def index(request, order=None):
-	tag = request.GET.get('tag')
 	context = dict(global_context)
+
+	questions_on_page = 20
+
+	tag = request.GET.get('tag')
+	page = request.GET.get('page')
+
 	if tag:
-		question_list = Question.newest_questions.filter(tags__title__exact=tag)
+		questions = Question.newest_questions.filter(tags__title__exact=tag)
 	else:
 		if not order or order == 'newest':
-			question_list = Question.newest_questions.all()
+			questions = Question.newest_questions.all()
 		elif order == 'best':
-			question_list = Question.best_questions.all()
+			questions = Question.best_questions.all()
 		else:
 			raise Http404
 
-	context.update( { 'question_list': question_list} )
+	paginator = Paginator(questions, questions_on_page)
+
+	try:
+		question_list = paginator.page(page)
+	except PageNotAnInteger:
+		question_list = paginator.page(1)
+	except EmptyPage:
+		question_list = paginator.page(paginator.num_pages)
+
+	context.update( { 'question_list': question_list } )
 	context.update( { 'order': order } )
 	context.update( { 'tag': tag } )
+
 	response = render(request, 'index.html', context)
 	return response
 
